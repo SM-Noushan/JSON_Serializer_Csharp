729b7919c7ec07d9780b9f7666ebb416212d3d0c
SMNoushan <noushan.md@gmail.com>
2026-09-10 22:03:14 +0600

    serialize int

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using tests;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Test cases for SerializeInt
+        var testInt = new TestInt();
+
+        testInt.SerializeInt(0, "0");
+        testInt.SerializeInt(42, "42");
+        testInt.SerializeInt(-25, "-25");
+        testInt.SerializeInt(int.MaxValue, "2147483647");
+        testInt.SerializeInt(int.MinValue, "-2147483648");
+
+    }
+}
---
+++ lib/JsonSerializer.cs
@@ -0,0 +1,9 @@
+namespace lib;
+
+public static class JsonSerializer
+{
+    public static string Serialize(int value)
+    {
+        return value.ToString();
+    }
+}
